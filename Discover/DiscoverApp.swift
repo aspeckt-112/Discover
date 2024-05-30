90cc7ea8529787a055f2d40f947f17699162dd67
aspeckt112 <little.koala5344@fastmail.com>
2024-05-30 16:53:06 +0100

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Russell Garrett on 30/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
